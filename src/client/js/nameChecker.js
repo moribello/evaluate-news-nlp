7f7b41b3bfc122a0edbf9da20e9ed7694fcac973
Mark Oribello <moribello@gmail.com>
2020-10-08 10:04:00 -0400

    feat: Continue to work on API functionality

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -11,6 +11,10 @@ function checkForName(inputText) {
     if(names.includes(inputText)) {
         alert("Welcome, Captain!")
     }
+    else{
+        let APIkey = "cdbb62a839e9a2011ce1b96497dda5af"
+        alert(`https://api.meaningcloud.com/sentiment-2.1?key=${APIkey}&of=json&txt=${inputText}&model=general&lang=en`)
+    }
 }
 
 export { checkForName }
